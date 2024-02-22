fb9d7754e966b40763fb355b8610f448c3db6b9f
kaihuanhuang <huangkaihuan0216@gmail.com>
2024-02-22 10:55:51 -0800

    review sliding window algorithms

--- leetcode-master/Algorithms/silding_window.py
+++ leetcode-master/Algorithms/silding_window.py
@@ -25,3 +25,28 @@ def closeDuplicates(nums, k):
     return False
 
 '''Q: Find the length of the longest subarray, with the same value in each position'''
+# Find the length of longest subarray with the same value in each position: O(n)
+def longestSubarray(nums):
+    len = 0
+    l = 0
+    
+    for r in range(len(nums)):
+        if nums[l] != nums[r]:
+            l = r
+        len = max(len, r - l + 1)
+    return len
+
+'''Q: Find the minimun length subarray, where the sum is greater than or equal to the target. Assume all vlaues are positive'''
+# O(n)
+def shortestSubarray(nums, target):
+    l, w_sum = 0, 0 #w_sum = window sum
+    len = float("inf")
+    
+    for r in range(len(nums)):
+        w_sum += nums[r]
+        while w_sum >= target:
+            len = min(r - l + 1, len)
+            w_sum -= nums[l]
+            l += 1
+        
+    return 0 if len == float("inf") else len
