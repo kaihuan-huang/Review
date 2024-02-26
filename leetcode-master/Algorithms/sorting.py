355e337a5769bb7c3233baf432b76cd2f78dba90
kaihuanhuang <huangkaihuan0216@gmail.com>
2024-02-26 10:34:32 -0800

    updated Sorting - Merge Sort

--- leetcode-master/Algorithms/sorting.py
+++ leetcode-master/Algorithms/sorting.py
@@ -8,3 +8,19 @@ for (i from 1 to arr.length){
         j -= 1
     }
 }
+
+# Sorting - Merge Sort - two branch recursion - Divide & Conquer
+# TC: O(nlogn) SC: O(n)
+# common used algorithms
+int[]
+def mergeSort(int[] arr, int s, int e){
+    if (e - s + 1 <= 1)
+    return arr;
+    
+    m = (s + e) / 2;
+    mergeSort(arr, s, m);
+    mergeSort(arr, m + 1, e);
+    
+    merge(arr, s, m, e);
+    return arr;
+}
