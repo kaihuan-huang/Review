d3d1f5206b926bdd3cc9def5a7687f2b5c30bb7d
kaihuanhuang <huangkaihuan0216@gmail.com>
2024-03-01 10:58:04 -0800

    reviewed permutations algorithms

--- leetcode-master/Algorithms/DFS/backtracking.py
+++ leetcode-master/Algorithms/DFS/backtracking.py
@@ -98,3 +98,36 @@ def helper3(i, curComb, combs, n, k):
         curComb.append(j)
         helper3(j + 1, curComb, combs, n, k)
         curComb.pop()
+        
+'''Permutations:
+Q: Given a list of nums, return all possible distinct permutations of nums.
+# Time Complexity: O(n^2 * n!)
+'''
+def permutationsRecursive(nums):
+    return helper4(0, nums)
+def helper4(i, nums):
+    if i == len(nums):
+        return[[]]
+    
+    resPerms = []
+    perms = helper(i + 1, nums)
+    for p in perms:
+        for j in range(len(p) + 1):
+            pCopy = p.copy()
+            pCopy.insert(j, nums[i])
+            resPerms.append(pCopy)
+    return resPerms
+
+'''Permutations: Iterative solutions TC: O(n^2 * n!)'''
+def permutationsIterative(nums):
+    perms = [[]]
+    
+    for n in nums:
+        nextPerms = []
+        for p in perms:
+            for i in range(len(p) + 1):
+                pCopy = p.copy()
+                pCopy.insert(i, n)
+                nextPerms.append(pCopy)
+        perms = nextPerms
+    return perms
