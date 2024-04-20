d63f76cfd5040b436853fad9cca9326aa81c918e
kaihuanhuang <huangkaihuan0216@gmail.com>
2024-04-19 22:46:12 -0700

    updated algorithoms

--- triangle.py
+++ triangle.py
@@ -7,7 +7,12 @@
 def triangle():
     for i in range(1, 6): #12345 not include 6
         print('*' * i)
-
+        
+        
+        
+        
+        
+        
 
 ''' 20. Valid Parentheses
 S=([1}) output=true
